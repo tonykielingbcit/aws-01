1c71159c0844ec7159e9825b9b16e3a049b4ff30
Tony Kieling <tony.kieling@gmail.com>
2023-01-04 14:55:37 -0800

    Add a get and post request

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+
+const pokemons = [
+  {
+    id: 1,
+    name: "Pikachu",
+    type: "electric ⚡️",
+    level: 99,
+    image: "/pikachu.webp"
+  }
+]
+
+app.get("/api/pokemons", (req, res) => {
+  console.log("GET /api/pokemons")
+  res.send({pokemons: pokemons})
+});
+
+app.post("/api/pokemons", (req, res) => {
+  const data = req.body
+  console.log("POST /api/pokemons", data)
+  data.id = pokemons.length + 1
+  pokemons.push(data)
+  res.send(data)
+})
 
 app.get("/", (req, res) => {
-  console.log("GET /")
-  res.send("<h1>hello aws</h1>")
-})
+    console.log("general GET");
+    res.send({message: "all good"});
+});
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
